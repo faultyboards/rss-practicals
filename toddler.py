3a6de0a72af808df44c83c9e4b7c49963cba4e3b
aleSuglia <alessandro.suglia@gmail.com>
2017-10-10 19:31:10 +0100

    Improved POI recognition.

--- toddler.py
+++ toddler.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
-from vision import detected_colored_object, get_robot_position_from_camera
-
 __TODDLER_VERSION__ = "1.0.0"
 
-import datetime
 import time
-
+import numpy
 import cv2
+import datetime
 import numpy as np
-
+from vision import detected_colored_object
 
 # Hardware test code
 class Toddler:
     def __init__(self, IO):
-        print('I am a toddler playing in a sandbox')
+        print 'I am a toddler playing in a sandbox'
         self.IO = IO
         self.inp = [0, 0, 0, 0, 0, 0, 0, 0]
-        self._poi_color_lower_range = np.array([169, 100, 100], dtype=np.uint8)
-        self._poi_color_upper_range = np.array([189, 255, 255], dtype=np.uint8)
-        self._satellite_pose = (np.array([0, 0, 0], dtype=np.float32), np.array([0, 0], dtype=np.float32))
-        self._poi_detected = False
-        self._position_from_camera = None
-        
+        self._poi_color_lower = np.array([0, 0, 48], dtype=np.uint8)
+        self._poi_color_upper = np.array([0, 0, 195], dtype=np.uint8)
+
     def move(self, l, r):
         if not l and not r:
             return [0, 0]
@@ -32,26 +27,21 @@ class Toddler:
         if l and r:
             return [100, 100]
 
-    def _change_antenna_position(self):
-        pass
 
-    def sense(self):
-        return self.IO.getInputs()
+            # This is a callback that will be called repeatedly.
 
-    def plan(self, state):
-        pass
-
-    def act(self, mot, mot_prev, pos):
-
-        # detected POI, set up the antenna orientation towards the satellite
-        if self._poi_detected:
-            self._change_antenna_position()
-        else:
+    # It has its dedicated thread so you can keep block it.
+    def Control(self, OK):
+        mot = [False, False, False]
+        motPrev = [False, False, False]
+        pos = 180
+        while OK():
+            time.sleep(0.05)
             self.inp = self.IO.getInputs()
-            mot[1] = self.inp[2]
             mot[0] = self.inp[1]
+            mot[1] = self.inp[2]
             mot[2] = self.inp[3]
-            if mot[0] != mot_prev[0] or mot[1] != mot_prev[1]:
+            if mot[0] != motPrev[0] or mot[1] != motPrev[1]:
                 speed = self.move(mot[0], mot[1])
                 self.IO.setMotors(-speed[0], speed[1])
                 if mot[0]:
@@ -67,40 +57,31 @@ class Toddler:
                 self.IO.servoEngage()
                 pos = (pos + 3) % 360
                 self.IO.servoSet(abs(pos - 180))
-            if mot[2] != mot_prev[2] and not mot[2]:
+            if mot[2] != motPrev[2] and not mot[2]:
                 self.IO.servoDisengage()
-            mot_prev[0] = mot[0]
-            mot_prev[1] = mot[1]
-            mot_prev[2] = mot[2]
-
-    # It has its dedicated thread so you can keep block it.
-    def Control(self, OK):
-        mot = [False, False, False]
-        motPrev = [False, False, False]
-        pos = 180
-        while OK():
-            time.sleep(0.05)
-            self.act(mot, motPrev, pos)
+            motPrev[0] = mot[0]
+            motPrev[1] = mot[1]
+            motPrev[2] = mot[2]
 
     # This is a callback that will be called repeatedly.
     # It has its dedicated thread so you can keep block it.
     def Vision(self, OK):
         self.IO.cameraSetResolution('low')
         hasImage = False
-        res = 0
-        sw = False
-        swPrev = False
         while OK():
-            if self.inp[4]:
-                for i in range(0, 5):
-                    self.IO.cameraGrab()
-                img = self.IO.cameraRead()
-                self._poi_detected = detected_colored_object(
-                    img,
-                    self._poi_color_lower_range,
-                    self._poi_color_upper_range
-                )
+            for i in range(0, 5):
+                self.IO.cameraGrab()
+            img = self.IO.cameraRead()
+            if img.__class__ == numpy.ndarray:
+                hasImage = True
 
-                self._position_from_camera = get_robot_position_from_camera(img)
+            if hasImage:
+                cv2.imwrite('camera-' + datetime.datetime.now().isoformat() + '.png', img)
+                self.IO.imshow('window', img)
+                self.IO.setStatus('flash', cnt=2)
+                detected, mask = detected_colored_object(img, self._poi_color_lower, self._poi_color_upper)
+                cv2.imshow('frame', mask)
+                if detected:
+                    cv2.imwrite('detected_colored_object-' + datetime.datetime.now().isoformat() + '.png', img)
 
             time.sleep(0.05)
