56cd33a92efbe2883849e518df94b1cf527a7838
Kuba Sanak <tech@kuba.fyi@lsgit config --global user.email tech@kuba.fyi@>
2017-10-25 16:43:32 +0100

    Fix a bunch of incorrect syntax

--- motion.py
+++ motion.py
@@ -8,7 +8,7 @@ class Motion():
 	'''
 	This class is a high-level motion controller dealing with odometry, navigation, etc.
 	'''
-	def __init__(IO, sensors):
+	def __init__(self, IO):
 		# Multiplier dealing with how the state of the battery affects distance-travelled
 		# estimates
 		self.avg_speed = 1
@@ -17,14 +17,16 @@ class Motion():
 		self.sensors = Sensors(self.IO)
 		self.motors = Motors(self.IO)
 
-		self.hall_trig_dist = 1
+		self.hall_trig_dist = 0.125
 		self.hall_timer = None
 		self.hall_count = 0
-		self.hall_reading_prev
+		self.hall_reading_prev = False
 
-		self.angle_time_multiplier = 8 # Needs to be callibrated
+		self.angle_time_multiplier = 1 # Needs to be callibrated
 
-	def _hall_handler():
+		self.i = 0
+
+	def _hall_handler(self):
 		'''
 		Called each time step to check what's up with teh hall sensor.
 		Deals with updating the battery_state estimation which then in turn is used to estimate
@@ -35,26 +37,26 @@ class Motion():
 		time_now = time.time()
 		if not hall_reading and self.hall_reading_prev:
 			self.hall_count += 1
+			self.i += 1
 			if self.hall_count > 1:
 				new_avg_speed = self.hall_trig_dist / (time_now - self.hall_timer)
-				if self.avg_speed_last_update is not None and np.abs(new_avg_speed-self.avg_speed) > 0.1 self.avg_speed:
+				if (self.avg_speed_last_update is not None) and (np.abs(new_avg_speed-self.avg_speed) > 0.1 * self.avg_speed):
 					raise Warning("Average speed estimate changed by more than 10%! (Hall sensor problem?)")
 				self.avg_speed = new_avg_speed
 				self.avg_speed_last_update = time_now
-			else:
 				
 			self.hall_timer = time_now
 
-			self.hall_reading_prev = hall_reading
+		self.hall_reading_prev = hall_reading
 
-	def _hall_handler_reset():
+	def _hall_handler_reset(self):
 		self.hall_trig_dist = 1
 		self.hall_timer = None
 		self.hall_count = 0
 		self.sensors.update_readings(type='digital')
 		self.hall_reading_prev = self.sensors.get_hall()
 
-	def move(amount, amount_type='distance'):
+	def move(self, amount, amount_type='distance'):
 		'''
 		Moves the robot by the specified amount. By default the amount is distance in meteres.
 		Set amount_type to 'time' for the amount to be time of motion in seconds, or 'callback'
@@ -67,23 +69,23 @@ class Motion():
 		start_time = time.time()
 		amount_travelled = 0
 
-		if amount_type = 'radians':
+		if amount_type == 'distance':
 			max_time = np.abs(amount) * self.angle_time_multiplier / self.avg_speed
-		elif amount_type = 'degrees':
-			max_time = np.abs(amount) * self.angle_time_multiplier / self.avg_speed * np.pi / 180
 		elif 'time':
-			max_time = time
+			max_time = time.time()
 
 		self.motors.full_on('forward' if amount > 0 else 'backwards')
 
 		while time.time() - start_time < max_time:
+			print('{}\t{}'.format(time.time() - start_time, max_time))
 			self._hall_handler()
 			amount_travelled = (time.time() - start_time) * self.avg_speed / self.angle_time_multiplier
 
+		self.motors.stop()
 		return amount_travelled
 
 
-	def turn(amount, amount_type='radians'):
+	def turn(self, amount, amount_type='radians'):
 		'''
 		Truns the robot by the specified amount. By default the amount is an angle in radians.
 		Set amount_type to 'degs' for imput in degrees, 'time' for the amount to be time of
@@ -94,10 +96,10 @@ class Motion():
 		'''
 		start_time = time.time()
 		amount_travelled = 0
-
-		if amount_type = 'radians':
+		print('{}\t{}\t{}'.format(np.abs(amount), self.angle_time_multiplier, self.avg_speed))
+		if amount_type == 'radians':
 			max_time = np.abs(amount) * self.angle_time_multiplier / self.avg_speed
-		elif amount_type = 'degrees':
+		elif amount_type == 'degrees':
 			max_time = np.abs(amount) * self.angle_time_multiplier / self.avg_speed * np.pi / 180
 		elif 'time':
 			max_time = time
@@ -105,6 +107,8 @@ class Motion():
 		self.motors.full_on('right' if amount > 0 else 'left')
 
 		while time.time() - start_time < max_time:
+			print('{}\t{}'.format(time.time() - start_time, max_time))
 			amount_travelled = (time.time() - start_time) * self.avg_speed / self.angle_time_multiplier
 
+		self.motors.stop()
 		return amount_travelled
