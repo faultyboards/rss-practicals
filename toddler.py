53550c13afb8e86bf7481bed9ee555e8ce8a6f83
Kuba Sanak <tech@kuba.fyi@lsgit config --global user.email tech@kuba.fyi@>
2017-10-26 12:04:00 +0100

    Tweaks to angle calculation

--- toddler.py
+++ toddler.py
@@ -6,15 +6,15 @@ __TODDLER_VERSION__ = "Best One"
 import numpy as np
 
 from sensors import Sensors
+from motors import Motors
 
 SATELLITE_POSITION = [-0.69, 0, 2.95]
 ANTENNA_POSITION = [-0.04, -0.09, 0.25]
-# ROBOT_POSITION = [0.355, 3.319, 0]
+ROBOT_POSITION = [0.355, 3.319, 0]
 # ROBOT_POSITION = [-0.9, 2.44, 0]
-ROBOT_POSITION = [-0.69, 0, 0]
-
+# ROBOT_POSITION = [0, 0, 0]
 # ROBOT_POSITION = [1.4, 1.8, 0]
-ROBOT_ORIENTATION = 0
+ROBOT_ORIENTATION = -np.pi/2
 
 
 def find_satellite(robot_position, robot_orientation, satellite_pos):
@@ -23,14 +23,16 @@ def find_satellite(robot_position, robot_orientation, satellite_pos):
     r = s - p
     r_f = np.concatenate([r[:2], np.zeros(1)])
 
-    ant_angl = np.pi/2 - np.arccos(np.dot(r,np.array([0,0,1]))/np.linalg.norm(r))
-    bot_turn_deg = robot_orientation + np.arccos(np.dot(r_f,np.array([1,0,0]))/np.linalg.norm(r_f)) + np.pi/2
+    ant_angl = (np.pi/2 - np.arccos(np.dot(r,np.array([0,0,1]))/np.linalg.norm(r))) % (2 * np.pi)
+    bot_turn_deg = ((1 if r_f[1] < 0 else -1) * np.arccos(np.dot(r_f,np.array([1,0,0]))/np.linalg.norm(r_f)) + \
+                    np.pi/2 - \
+                    robot_orientation) % (2 * np.pi)
     
     if bot_turn_deg < -np.pi/2:
         bot_turn_deg += np.pi
-        ant_angl = np.pi - ant_angl
     elif bot_turn_deg > np.pi/2:
         bot_turn_deg -= np.pi
+    else:
         ant_angl = np.pi - ant_angl
 
     return bot_turn_deg, ant_angl
@@ -41,19 +43,22 @@ class Toddler:
         self.IO = IO
         self.sensors = Sensors(self.IO)
         self.motion = Motion(self.IO)
+        self.motors = Motors(self.IO)
 
     # It has its dedicated thread so you can keep block it.
     def Control(self, OK):
         goal_reached = False
+        # self.motors.full_on('forward')
         while OK():
             if not goal_reached:
                 bot_turn_deg, antenna_rot = find_satellite(ROBOT_POSITION, ROBOT_ORIENTATION, SATELLITE_POSITION)
                 print bot_turn_deg * 180 / np.pi, antenna_rot  * 180 / np.pi
 
-                self.motion.move(0.30)
-                self.motion.move(-0.30)
-                self.motion.turn(bot_turn_deg)
-                self.motion.set_antenna(antenna_rot)
+                self.motion.move(0.297)
+                self.motion.move(-0.297)
+                self.motion.turn(45, 'degrees')
+                self.motion.set_antenna(0)
+                self.motion.set_antenna(180)
                 goal_reached = True
 
     # This is a callback that will be called repeatedly.
--- toddler_satellite.py
+++ toddler_satellite.py
@@ -6,81 +6,36 @@ __TODDLER_VERSION__ = "Best One"
 import numpy as np
 
 from sensors import Sensors
+from motors import Motors
 
 SATELLITE_POSITION = [-0.69, 0, 2.95]
 ANTENNA_POSITION = [-0.04, -0.09, 0.25]
-# ROBOT_POSITION = [0.355, 3.319, 0]
+ROBOT_POSITION = [0.355, 3.319, 0]
 # ROBOT_POSITION = [-0.9, 2.44, 0]
-ROBOT_POSITION = [1.4, 1.8, 0]
-ROBOT_ORIENTATION = np.pi
+# ROBOT_POSITION = [0, 0, 0]
+# ROBOT_POSITION = [1.4, 1.8, 0]
+ROBOT_ORIENTATION = -np.pi/2
 
 
 def find_satellite(robot_position, robot_orientation, satellite_pos):
-    # rotate the antenna to horizontal
-    forwardVec = [1, 0]
+    p = np.array(robot_position)
+    s = np.array(satellite_pos)
+    r = s - p
+    r_f = np.concatenate([r[:2], np.zeros(1)])
 
-    antennaPos = np.add(robot_position, ANTENNA_POSITION)
+    ant_angl = (np.pi/2 - np.arccos(np.dot(r,np.array([0,0,1]))/np.linalg.norm(r))) % (2 * np.pi)
+    bot_turn_deg = ((1 if r_f[1] < 0 else -1) * np.arccos(np.dot(r_f,np.array([1,0,0]))/np.linalg.norm(r_f)) + \
+                    np.pi/2 - \
+                    robot_orientation) % (2 * np.pi)
+    
+    if bot_turn_deg < -np.pi/2:
+        bot_turn_deg += np.pi
+    elif bot_turn_deg > np.pi/2:
+        bot_turn_deg -= np.pi
+    else:
+        ant_angl = np.pi - ant_angl
 
-    antenna2Satellite = np.subtract(satellite_pos, antennaPos)
-
-    # normalise
-    np.linalg.norm(antenna2Satellite)
-
-    print "antenna2Satellite = ", antenna2Satellite
-
-    # calculate the vector direction of the bot using its angle from global forward
-    botDir = [np.add(np.multiply(forwardVec[0], np.cos(robot_orientation)),
-                     np.multiply(forwardVec[1], np.sin(robot_orientation))),
-              np.add(np.multiply(-forwardVec[0], np.sin(robot_orientation)),
-                     np.multiply(forwardVec[1], np.cos(robot_orientation))),
-              0]
-
-    # normalise
-    np.linalg.norm(botDir)
-
-    print "\nbotdir = ", botDir
-
-    # the x& y values of antenna to satellite
-    a2sg = [antenna2Satellite[0], antenna2Satellite[1], 0]
-    # a2sg converted into 3d space by adding a 0 in the z axis
-    a2s3d = [antenna2Satellite[0], antenna2Satellite[1], 0]
-
-    upVector = [0,0,1]
-    normal = np.cross(botDir, a2sg)
-    print 'dotDir = ', botDir
-    print ' a2sg= ', a2sg
-    angle = np.dot(upVector, normal)
-
-    print 'angle = ', angle
-
-    sign = angle / np.abs(angle)
-
-    print 'sign = ', sign
-
-
-    # find the angle between the bots forward direction and the satellite
-    botTurnRad = np.arccos(np.divide(np.dot(botDir, a2sg),
-                                     np.multiply(sum(np.multiply(botDir, botDir)) ** 0.5,
-                                                 sum(np.multiply(a2sg, a2sg)) ** 0.5)))
-
-    # convert the angle to degrees
-    botTurnDeg = (botTurnRad / (2 * np.pi)) * 360
-
-   #botTurnDeg = botTurnDeg * sign
-
-
-    # find the angle between the plane and the satellite
-    antennaRot = np.arccos(np.divide(np.dot(a2s3d, antenna2Satellite),
-                                     np.multiply(sum(np.multiply(a2s3d, a2s3d)) ** 0.5,
-                                                 sum(antenna2Satellite * antenna2Satellite) ** 0.5)))
-
-    # convert to degrees
-    antennaRot = (antennaRot / (2 * np.pi)) * 360
-
-    print antennaRot
-    print "\n", botTurnDeg
-    return botTurnDeg, antennaRot
-
+    return bot_turn_deg, ant_angl
 
 class Toddler:
     def __init__(self, IO):
@@ -88,20 +43,22 @@ class Toddler:
         self.IO = IO
         self.sensors = Sensors(self.IO)
         self.motion = Motion(self.IO)
+        self.motors = Motors(self.IO)
 
     # It has its dedicated thread so you can keep block it.
     def Control(self, OK):
         goal_reached = False
+        # self.motors.full_on('forward')
         while OK():
             if not goal_reached:
                 bot_turn_deg, antenna_rot = find_satellite(ROBOT_POSITION, ROBOT_ORIENTATION, SATELLITE_POSITION)
-                #if np.abs(bot_turn_deg) >= 180:
-                #    bot_turn_deg = (bot_turn_deg+180)%360
-                self.motion.move(0.20)
-                self.motion.move(-0.20)
-                if ROBOT_ORIENTAT
-                self.motion.turn(bot_turn_deg - 90, 'degrees')
-                self.motion.set_antenna(antenna_rot, 'degrees')
+                print bot_turn_deg * 180 / np.pi, antenna_rot  * 180 / np.pi
+
+                self.motion.move(0.297)
+                self.motion.move(-0.297)
+                self.motion.turn(45, 'degrees')
+                self.motion.set_antenna(0)
+                self.motion.set_antenna(180)
                 goal_reached = True
 
     # This is a callback that will be called repeatedly.
