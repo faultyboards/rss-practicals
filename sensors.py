2cc3d3ef22a2d0ea19b2ea9bc796ee2061090ab3
Kuba Sanak <tech@kuba.fyi@lsgit config --global user.email tech@kuba.fyi@>
2017-11-09 19:39:56 +0000

    Mid-work, moveing pcs

--- sensors.py
+++ sensors.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Sensors:
     def __init__(self, IO):
         self.IO = IO
@@ -65,7 +67,14 @@ class Sensors:
 
     def get_ir(self, sensor_loc, raw=False):
         if not raw:
-            reading = self.analogue_readings[self.port['ir'][sensor_loc]]
+            samples_no = 1000
+            readings = np.empty(samples_no)
+            for i in range(samples_no):
+                readings[i] = self.analogue_readings[self.port['ir'][sensor_loc]]
+            r_med = np.median(readings)
+            r_std = readings.std()
+            readings_no_outliers = [reading for reading in readings if np.abs(r_med-reading) < 2 * r_std]
+            reading = np.median(readings_no_outliers)
             return (self.ir_dist_mltpl[0] +
                     reading * self.ir_dist_mltpl[1] +
                     reading * reading * self.ir_dist_mltpl[2])
--- toddler.py
+++ toddler.py
@@ -4,7 +4,10 @@ from motors import Motors
 from sensors import Sensors
 from wallwalker import Wallwalker
 
+import numpy as np
+
 __TODDLER_VERSION__ = "Best One"
+
 
 class Toddler:
     def __init__(self, IO):
@@ -15,12 +18,21 @@ class Toddler:
         self.motors = Motors(self.IO)
         self.wallwalker = Wallwalker(self.sensors, self.motion)
 
+        self.SAMPL = 100
+
+        self.readings_idx = 0
+        self.readings = np.empty(self.SAMPL)
+
     # It has its dedicated thread so you can keep block it.
     def Control(self, OK):
         while OK():
-            self.wallwalker.step()
-            print(self.wallwalker.distance_along)
-            time.sleep(2)
+            # self.wallwalker.step()
+            # print(self.wallwalker.distance_along)
+            # time.sleep(2)
+            reading = self.sensors.get_ir('right')
+            self.readings[self.readings_idx] = reading
+            self.readings_idx = (self.readings_idx + 1) % self.SAMPL
+            print('mean: {}\tsd: {}'.format(np.median(self.readings), self.readings.std()))
 
     # This is a callback that will be called repeatedly.
     # It has its dedicated thread so you can keep block it.
